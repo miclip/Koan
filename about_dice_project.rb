81e56d93cf5694c1966b59af025305820b214ae4
Michael Lipscombe <mlipscombe@outlook.com>
2016-11-17 20:58:46 -0500

    finished

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  attr_reader :values 
+ 
+  def roll(value)
+    @values = Array.new
+    value.times do |roll|
+      self.values[roll] = 1 + rand(6)
+    end 
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,29 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+    attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = Array.new
   end
 
-  # WRITE CODE HERE
+ def method_missing(method_name, *args, &block)
+    @messages << method_name
+    if @object.respond_to?(method_name.to_s)
+      @object.__send__(method_name, *args, &block)
+    else
+      super(method_name, *args, &block)
+    end
+  end
+
+  def called?(method_name)
+    @messages.include? method_name
+  end
+
+  def number_of_times_called(method_name)
+    @messages.find_all{|method| method == method_name}.count
+  end
 end
 
 # The proxy object should pass the following Koan:
